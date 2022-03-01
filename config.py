923545c51d63ae073e68ca34895b1c4e7c7487e7
TUSHAR KASAUDHAN <69419073+tushar0305@users.noreply.github.com>
2022-03-01 13:06:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cbh9no9tsabi.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "abhay234"
+customdb = "employee"
+custombucket = "addemployee0103"
+customregion = "us-east-1"
